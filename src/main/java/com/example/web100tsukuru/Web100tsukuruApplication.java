10b8b262c697e92f7598e3d3c4e347621bcacf66
tool1102-source <tool.github.1102@gmail.com>
2025-11-14 21:10:53 +0900

    feat: Spring Bootプロジェクトにインデックスページを組み込み、Thymeleafに対応

---
+++ src/main/java/com/example/web100tsukuru/Web100tsukuruApplication.java
@@ -0,0 +1,13 @@
+package com.example.web100tsukuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web100tsukuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web100tsukuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web100tsukuru/Web100tsukuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web100tsukuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web100tsukuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
